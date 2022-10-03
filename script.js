52bdbbe37b6010b897c8d97fd1e46d7ccd5c4a30
unknown <alejonoviello@gmail.comgit config --global user.name alejo>
2022-10-03 20:22:37 -0300

    Tp js

---
+++ script.js
@@ -0,0 +1,33 @@
+//#region variable
+var nombre1="hola"
+let nombre2='chau'
+const nombre3=`gh`;
+//#endregion variable
+//#region console
+
+//#endregion console
+//#region for
+var inicio=0;
+var fin=[10];
+for(x=0;x<10;x++){
+console.log(nombre3)
+}
+for(inicio of fin){
+    console.log(nombre1);
+    console.log(nombre2);
+
+}
+//#endregion for
+//#region function
+var num1=4;
+var num2=4;
+var multi=0;
+var lol=(num)=>{
+    var multi= num1*num2;
+return multi;
+}
+console.log(multi)
+//#endregion function
+
+// iterable=array
+
